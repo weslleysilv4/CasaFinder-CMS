459379ec32500e2c4821b7d3cddd10addcc14d35
weslleysilv4 <weslleysilva.swe@gmail.com>
2024-04-13 00:57:02 -0300

    feat: inittial folder structure

---
+++ App.js
@@ -0,0 +1,35 @@
+//Requires gerais
+const path = require("path")
+require("dotenv").config()
+
+//Express
+const express = require('express')
+const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Template
+var mustacheExpress = require("mustache-express");
+var engine = mustacheExpress()
+app.engine("mustache", engine);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "mustache");
+
+//Cookies
+const cookieParser = require("cookie-parser")
+app.use(cookieParser())
+
+//Sessão
+const session = require("express-session")
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use("/", require('./src/controllers/main'))
+
+app.listen(process.env.PORT, () => {
+    console.log("Running...")
+})
