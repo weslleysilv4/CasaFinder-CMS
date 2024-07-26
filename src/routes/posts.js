211e317438e737cf5b77cf15d03a6e4230dd6622
Weslley Silva <weslleysilva.swe@gmail.com>
2024-07-26 14:15:37 -0300

    chore: Update sidebar links to use /dashboard instead of /admin

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -5,4 +5,8 @@ router.route('/dashboard').get(Access.isAuthenticated, (req, res) => {
   res.render('admin/index')
 })
 
+router.get('/dashboard/posts', Access.isAuthenticated, (req, res) => {
+  res.render('admin/pages/anuncios')
+})
+
 module.exports = router
