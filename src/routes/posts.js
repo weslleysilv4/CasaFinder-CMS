15b13ca4be3f7101aab966be65d89ae143b43681
Weslley Silva <weslleysilva.swe@gmail.com>
2024-07-27 15:09:07 -0300

    chore: Update dashboard posts route to fetch data from the database

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,13 +1,19 @@
 const router = require('express').Router()
 const Access = require('../controllers/accessController')
 const postController = require('../controllers/postController')
+const db = require('../controllers/dbController')
 
 router.get('/dashboard', Access.isAuthenticated, (req, res) => {
   res.render('admin/index')
 })
 
-router.get('/dashboard/posts', Access.isAuthenticated, (req, res) => {
-  res.render('admin/pages/anuncios')
+router.get('/dashboard/posts', Access.isAuthenticated, async (req, res) => {
+  const data = Access.isAdmin(req.session.user)
+    ? await db.getAllPosts()
+    : await db.getAllPostsByUser(req.session.user.email)
+  res.render('admin/pages/anuncios', {
+    data: data,
+  })
 })
 
 router.get('/dashboard/posts/new', Access.isAuthenticated, (req, res) => {
