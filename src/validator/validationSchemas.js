5b36a0fbf5343c20356ce31d20c8d575200201c5
Leo Fasano <119373790+Fasano18@users.noreply.github.com>
2024-07-30 08:51:08 -0300

    Update validationSchemas.js
    Add URL

--- src/validator/validationSchemas.js
+++ src/validator/validationSchemas.js
@@ -1,4 +1,4 @@
-const Joi = require('joi')
+const Joi = require('joi');
 
 const createUserValidator = Joi.object({
   username: Joi.string().min(3).max(30).required(),
@@ -7,7 +7,7 @@ const createUserValidator = Joi.object({
     tlds: { allow: ['com', 'net'] },
   }),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-})
+});
 
 const createPostValidator = Joi.object({
   title: Joi.string().min(3).required(),
@@ -16,10 +16,11 @@ const createPostValidator = Joi.object({
   price: Joi.number().positive().required(),
   address: Joi.string().min(5).required(),
   imgURL: Joi.string().uri().optional(),
-  markdown: Joi.string().min(10).required()
-})
+  markdown: Joi.string().min(10).required(),
+  url: Joi.string().uri().optional()  // Adicionada a validação para URL
+});
 
 module.exports = {
   createUserValidator,
   createPostValidator,
-}
+};
