15f3e3bda9c90b617e91ba5444def5e3639fa714
Weslley Silva <weslleysilva.swe@gmail.com>
2024-07-31 12:06:46 -0300

    refactor: Simplificar código de busca de posts do usuário e atualização de postagens

--- src/validator/validationSchemas.js
+++ src/validator/validationSchemas.js
@@ -1,4 +1,4 @@
-const Joi = require('joi');
+const Joi = require('joi')
 
 const createUserValidator = Joi.object({
   username: Joi.string().min(3).max(30).required(),
@@ -7,7 +7,7 @@ const createUserValidator = Joi.object({
     tlds: { allow: ['com', 'net'] },
   }),
   password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-});
+})
 
 const createPostValidator = Joi.object({
   title: Joi.string().min(3).required(),
@@ -17,10 +17,10 @@ const createPostValidator = Joi.object({
   address: Joi.string().min(5).required(),
   imgURL: Joi.string().uri().optional(),
   markdown: Joi.string().min(5).optional(),
-  url: Joi.string().required()  
-});
+  url: Joi.string().required().min(4),
+})
 
 module.exports = {
   createUserValidator,
   createPostValidator,
-};
+}
